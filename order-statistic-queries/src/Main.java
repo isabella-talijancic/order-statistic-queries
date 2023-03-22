7b82dcc837fc989aad6267b0fc47d850371ba471
Isabella <66403691+isabella-talijancic@users.noreply.github.com>
2023-03-22 01:19:17 -0500

    first commit

---
+++ order-statistic-queries/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+/**
+ * Main is a Java class.
+ * 
+ * @author Isabella Talijancic (juu530)
+ * @author Amalia Talijancic (fwn783)
+ * UTSA CS 3343 - Project 2
+ * Spring 2023
+ */
+
+public class Main {
+	
+}
